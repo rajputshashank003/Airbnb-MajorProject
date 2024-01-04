e8d0e57549ce5fdeba6bbd00757da2c8032c76a1
Shashank Rajput <hariomshashank@gmail.com>
2024-01-04 16:31:21 +0530

    Add Project Files

---
+++ app.js
@@ -0,0 +1,175 @@
+if(process.env.NODE_ENV != "production"){
+    require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+
+// here we used MVC FRAMEWORK MODELS VIEWS AND CONTROLLERS MENTION IT ON RESUME 
+
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");// help to create layouts or templates 
+
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const MongoStore = require("connect-mongo");
+
+const {listingSchema , reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
+
+const listingRouter = require("./routes/listing.js"); 
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+
+const session = require("express-session");
+const flash = require("connect-flash");
+
+const path = require("path");
+
+const passport = require("passport"); // sessions are required for passport because in same session user doesn't have to register again 
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+const Listing = require("./models/listing");
+
+const { rmSync } = require("fs");
+const { serialize } = require("v8");
+app.set("view engine" ,"ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname,"public")));   
+
+const dbUrl = process.env.ATLASDB_URL; 
+
+
+
+// app.get("/" ,(req,res) => { 
+//     res.send("hii ! i am root ");
+// }); 
+
+const store = MongoStore.create({
+    mongoUrl : dbUrl,
+    crypto : {
+        secret : process.env.SECRET,
+    },
+    touchAfter : 24 * 3600 ,
+});
+
+store.on("error" , () => {
+    console.log("ERROR in mongo-session-store" , error);
+});
+
+const sessionOptions = {
+    store ,
+    secret: process.env.SECRET,
+    resave :false,
+    saveUninitialized : true,
+    cookie : {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge : 7 * 24 * 60 * 60 * 1000,
+        httpOnly : true, 
+    },
+};
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session()); // identify same user on different page 
+passport.use(new LocalStrategy(User.authenticate())); 
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res ,next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
+
+// app.get("/demouser", async (req,res) => {
+//     let fakeUser = new User({
+//         email : "student@gmail.com",
+//         username : "delt-student",
+//     });
+
+//     let registeredUser = await User.register(fakeUser, "hello"); // also check username is unique or not !
+//     res.send(registeredUser);    
+// });
+
+
+
+app.use("/listings" , listingRouter);
+app.use("/listings/:id/reviews" , reviewRouter);
+app.use("/" , userRouter);
+app.post("/searched" , async (req, res) => {
+    function checkFor ( str1,str2) {
+        const lowerCaseStr1 = str1.toLowerCase();
+        const lowerCaseStr2 = str2.toLowerCase();
+
+        // Remove spaces from both strings
+        const noSpaceStr1 = lowerCaseStr1.replace(/\s/g, '');
+        const noSpaceStr2 = lowerCaseStr2.replace(/\s/g, '');
+
+        // Compare the two strings
+        return noSpaceStr1 === noSpaceStr2;
+    };
+    
+    const input = req.body.input;
+
+    let allLists = await Listing.find({}); 
+    // console.log(allLists);
+    let count2 = 0;
+    let filteredLists  = [];
+    
+    for(list of allLists){
+        let i = input.toLowerCase();
+        l = list.country.toLowerCase();
+        let count = 0;
+        if(checkFor(l,i)){
+            filteredLists.push(list);
+        }
+    }
+    allLists = filteredLists;
+    if(allLists.length == 0){
+        req.flash("error" , "No listing found !");
+        res.redirect("/listings");
+    } else {
+        res.render("listings/index.ejs", {allLists});
+    }
+});
+
+
+
+main()
+    .then( () => { 
+        console.log("connected to database");
+    }) 
+    .catch(err=> { 
+        console.log(err);
+    });
+
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+
+app.all("*",(req,res,next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+})
+app.use((err,req,res,next) => {
+    let {status=500 , message ="something went wrong!"} = err;
+    res.status(status).render("error.ejs",{message});
+    // res.status(status).send(message );
+});
+
+
+app.listen(8080, () => {
+    console.log("server listening on port 8080");
+});
---
+++ routes/listing.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const {listingSchema , reviewSchema } = require("../schema.js");
+const Listing = require("../models/listing.js");
+const {isLoggedIn , isOwner , validateListing} = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer( { storage } );
+
+// router.route() use to combine two route with same link
+
+router
+    .route("/")
+    .get(wrapAsync(listingController.index)) //index route 
+    .post(                                      // create route
+        isLoggedIn,
+        upload.single("listing[image]"),
+        validateListing,
+        wrapAsync(listingController.createListing)
+    );
+
+
+
+// to make new route listing here isloggesin works as middleware for checking authentication 
+router.get("/new" , isLoggedIn , listingController.renderNewForm);
+
+router
+    .route("/:id")
+    .get(wrapAsync(listingController.showListing)) // show route : print the data of any individual listing 
+    .put(                                          // update route
+        isLoggedIn ,
+        isOwner,
+        upload.single("listing[image]"),
+        validateListing ,
+        wrapAsync(listingController.updateListing)
+    )
+    .delete(                                      // delete route 
+        isLoggedIn,
+        isOwner,
+        wrapAsync(listingController.destroyListing)
+    );
+
+
+// index route
+// router.get("/" ,wrapAsync(listingController.index));
+
+// edit route
+router.get("/:id/edit",
+    isLoggedIn ,
+    isOwner,
+    wrapAsync(listingController.renderEditForm)
+);
+
+
+//create route 
+// router.post(
+//     "/" ,
+//     isLoggedIn,
+//     validateListing,
+//     wrapAsync(listingController.createListing)
+// );
+
+
+
+
+module.exports = router;
