eef734757d88a2c20af16eb04325dbe53543cc38
rajputshashank003 <hariomshashank@gmail.com>
2025-11-27 22:42:29 +0530

    fix: temp-cors setup

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if(process.env.NODE_ENV != "production"){
     require("dotenv").config();
 }
-const cors = required('cors');
+const cors = require('cors');
 const express = require("express");
 const app = express();
 
